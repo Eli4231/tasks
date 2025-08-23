5c55dacf7f96fbfd0bd87e6c63ef8cb3155fe56b
Eli lang <elilang92@gmail.com>
2025-08-23 17:50:13 +0300

    Added delete of CRUD

--- index.js
+++ index.js
@@ -26,5 +26,14 @@ app.post('/t',(req,res)=>{
 res.status(201).json({message:"ok"})
 })
 
+app.delete('/t/:id',(req,res)=>{
+let id =req.params.id;
+if(id < 0 || tasks.length < id){
+    return res.status(400).json({message:"does not exist"})
+}
+tasks[id]=null;
+res.status(200).json({message:"Deleted!"})
+})
+
 
 app.listen(port,()=>{console.log(`http://localhost:${port}`)});
